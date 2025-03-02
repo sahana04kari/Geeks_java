b087f33e530924352bb1623a9133948cc4d054e6
Madhu <madhukariger20@gmail.com>
2025-03-02 15:14:43 +0530

    2 march

---
+++ src/keywords.java
@@ -0,0 +1,17 @@
+class keywords
+{
+    public static void main(String[] args)
+    {
+        // Using final and int keyword
+        final int x = 10;
+
+        // Using if and else keywords
+        if(x>10){
+            System.out.println("Failed");
+        }
+        else{
+            System.out.println("Succesful Demonstration"
+                    +" of keywords");
+        }
+    }
+}
